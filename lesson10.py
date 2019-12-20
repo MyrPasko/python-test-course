2c6a6710ccc824d1fc7b89f391337b477122e2cc
MyrPasko <myroslavpasko@gmail.com>
2019-12-20 23:40:06 +0200

    On lesson 11

---
+++ lesson10.py
@@ -0,0 +1,8 @@
+s = 'STRING'
+print(s.center(10, '?'))
+print(s.capitalize())
+spell = 'sabbra-cadabra'
+print(spell.count('ra', 0, 6))
+print(spell.find('ra'))
+print(spell.index('ra'))
+print(spell.swapcase())
---
+++ lesson11.py
@@ -0,0 +1,18 @@
+# String formatting
+
+name = 'Johny'
+age = 32
+title = 'Sony'
+price = 100
+
+print('My name is ' + name + ' and I\'m ' + str(age) + ' old.')
+print('My name is %(name)s and I\'m %(age)d old.' % {'name': name, 'age': age})
+print('My name is %s and I\'m %d old. %s' % (name, age, name))
+print('Title: %s, price: %.2f' % ('Sony', 100))
+
+# Modern method .format
+print('Title: {1}, price: {0}'.format(title, price))
+print('Title: {title}, price: {price}'.format(title=title, price=price))
+
+# F-strings
+print(f'Title: {title}, price: {price}')
